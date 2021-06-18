fc4001c9cfe5e7a3a8abe54f2e8727a32dcb190c
niranjan0908 <ninjaniranjan875@gmail.com>
2021-06-18 20:18:08 +0530

    ex5

--- EX_5/app/src/main/java/com/example/ex_5/MainActivity.java
+++ EX_5/app/src/main/java/com/example/ex_5/MainActivity.java
@@ -2,13 +2,46 @@ package com.example.ex_5;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.os.Build;
 import android.os.Bundle;
 
+
+import android.view.View;
+import android.widget.Button;
+
 public class MainActivity extends AppCompatActivity {
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button = findViewById(R.id.btn_show);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Notification();
+            }
+        });
+    }
+    private void Notification(){
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+            NotificationChannel notificationChannel =
+                    new NotificationChannel("n","n", NotificationManager.IMPORTANCE_DEFAULT);
+            NotificationManager manager = getSystemService(NotificationManager.class);
+            manager.createNotificationChannel(notificationChannel);
+        }
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this,"n")
+                .setContentText("Notification")
+                .setSmallIcon(R.drawable.ic_launcher_background)
+                .setAutoCancel(true)
+                .setContentText("Hello USER");
+        NotificationManagerCompat managerCompat = NotificationManagerCompat.from(this);
+        managerCompat.notify(999,builder.build());
     }
 }
