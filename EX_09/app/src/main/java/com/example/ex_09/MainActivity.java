b2e3cdb1615bb1d8ebdb3bb59999802b8892b9b1
niranjan0908 <ninjaniranjan875@gmail.com>
2021-06-29 14:42:51 +0530

    all expts done

---
+++ EX_09/app/src/main/java/com/example/ex_09/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.ex_09;
+
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
+import androidx.appcompat.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button notify;
+    EditText e;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        notify= (Button) findViewById(R.id.button);
+        e= (EditText) findViewById(R.id.editText);
+
+        notify.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+                PendingIntent pending = PendingIntent.getActivity(MainActivity.this, 0, intent, 0);
+                Notification noti = new Notification.Builder(MainActivity.this).setContentTitle("New Message").setContentText(e.getText().toString()).setSmallIcon(R.mipmap.ic_launcher).setContentIntent(pending).build();
+                NotificationManager manager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+                noti.flags |= Notification.FLAG_AUTO_CANCEL;
+                manager.notify(0, noti);
+            }
+        });
+    }
+}
---
+++ EX_09/app/src/main/java/com/example/ex_09/SecondActivity.java
@@ -0,0 +1,14 @@
+package com.example.ex_09;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+    }
+}
